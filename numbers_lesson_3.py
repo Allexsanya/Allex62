bd8d11537c9651b43ada2308e24c1af9ed4c98de
User <htfgf@gmail.com>
2025-04-28 20:48:42 -0700

    mirror added

---
+++ numbers_lesson_3.py
@@ -0,0 +1,10 @@
+some_number = "       3435  "
+# ............................................
+
+some_number = some_number.strip()
+
+some_number_mult_2 = some_number * 2
+print(some_number_mult_2)
+
+
+pass
