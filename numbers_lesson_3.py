e67ba9eb82a3180d44b1d886d6b4f123496378d2
User <htfgf@gmail.com>
2025-04-29 21:24:40 -0700

    mirror added

--- numbers_lesson_3.py
+++ numbers_lesson_3.py
@@ -2,9 +2,36 @@ some_number = "       3435  "
 # ............................................
 
 some_number = some_number.strip()
+some_number = int(some_number)
+
 
 some_number_mult_2 = some_number * 2
 print(some_number_mult_2)
 
+number_value = 5_45_4545_454
+number_mult = number_value * 5
+number_mult *= 2
+
+number_mult /= 4
+num1 = 0.1
+num2 = 0.2
+num3 = num1 + num2
+
+num4 = num1 - num3
+negative_number1 = -1
+negative_number2 = -156.124
+rounded_negative_number2_int = round(negative_number2, 2)
+negative_bad_number = -156.0
+
+power = 5**2
+root = 25**0.5
+
+div1 = 26 // 5
+rest = 26 % 5
+
+hw = 123235
+hw_1 = hw // 100_000
+hw_2 = (hw - hw_1 * 100_000) // 10_000
+reversed_value = hw_2 * 10000 + hw * 10000
 
 pass
---
+++ shop.py
@@ -0,0 +1,37 @@
+import decimal
+from pywebio.input import slider, FLOAT, NUMBER
+
+from pywebio.input import input as pw_input
+
+from pywebio.output import put_html, put_success
+
+CHEESE_PRICE = decimal.Decimal(286.35)
+POTATO_PRICE = decimal.Decimal(40.32)
+
+
+# HEADER
+put_html("<h1>Welcome to our shops</h1>")
+
+# INPUT SECTION
+
+cheese_weight = slider(
+    "Cheese", type=FLOAT, min_value=0, max_value=5, value=0.15, required=True
+)
+cheese_weight = decimal.Decimal(cheese_weight)
+    decimal.Decimal("target1").quantize
+)
+potato_weight = pw_input(
+    "Potato", type=NUMBER, required=True, min_value=0, max_value=10, value=3
+)
+potato_weight = decimal.Decimal(potato_weight)
+print("Selected cheese weight:", cheese_weight)
+print("Selected potato weight:", potato_weight)
+
+cheese_cost = cheese_weight * CHEESE_PRICE
+potato_cost = potato_weight * POTATO_PRICE
+
+total_cost = cheese_cost + potato_cost
+put_success(f"Total cost: {cheese_cost=} {potato_cost=} {total_cost}")
+
+
+pass
